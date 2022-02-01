30c196cd9d5c11578e0894f8b98a80370a9c54fa
Cole Peterson <rcolepeterson@gmail.com>
2022-01-31 19:08:58 -0800

    removed unused

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@
 
 import React, { useRef } from "react";
 import "./App.css";
-import * as tf from "@tensorflow/tfjs";
+// import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 import { drawKeypoints, drawSkeleton } from "./utilities";
